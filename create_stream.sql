054a2cd7de84fc05f9aa51d579c4e274f6071fc1
mrturkmen <hi@mrturkmen.com>
2022-07-17 22:43:41 +0200

    add create_stream commands

--- create_stream.sql
+++ create_stream.sql
@@ -4,4 +4,13 @@ CREATE STREAM meetup_events_stream ( utc_offset BIGINT, venue STRUCT <country VA
                                    `group` STRUCT <joined_mode VARCHAR, country VARCHAR, city VARCHAR, name VARCHAR, group_lon DOUBLE, id BIGINT, urlname VARCHAR,
                                    category STRUCT <name VARCHAR, id INT, shortname VARCHAR>,
                                    group_photo STRUCT <highres_link VARCHAR, photo_link VARCHAR, photo_id BIGINT, thumb_link VARCHAR>,
-                                   group_lat DOUBLE>, status VARCHAR) WITH (KAFKA_TOPIC='event-processing-project', VALUE_FORMAT='JSON');
+                                   group_lat DOUBLE>, status VARCHAR) WITH (KAFKA_TOPIC='the-meetup-events', VALUE_FORMAT='JSON');
+
+
+
+CREATE STREAM country_filtered AS
+    SELECT * FROM meetup_events_stream WHERE VENUE -> COUNTRY='de' EMIT CHANGES;
+
+
+CREATE STREAM city_filtered AS
+SELECT * FROM COUNTRY_FILTERED WHERE VENUE -> CITY='Munchen' EMIT CHANGES;
